9b04d9d9c57d1754a9ae28bca30213aab9eb8bee
wakoya-tech <waaqoyyaadhaabaa@gmail.com>
2025-12-13 21:46:25 +0300

    add npm setup and project files

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-const http = require('http');
-const server= http.createServer((req,res)=>{
-   if(req.url === '/'){
-    res.end('Welcome to our home page')
-   }
-    if(req.url === '/about')
-        {
-            req.end('Here is our short history')
-        }
-        res.end(`<h1>Oops!</h1>
-        <p>we can't seem to find the page you are looking for</p>
-       <a href="/">back home</a>`)
-    })
-server.listen(5000)
-
+const _= require('lodash');
+const items=[1,[2,[3[4]]]];
+const newItems=_.flattenDeep(items);
+console.log(newItems);
