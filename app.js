be22c773ff74601ee93e4e5f9415fbdeeba19709
wakoya-tech <waaqoyyaadhaabaa@gmail.com>
2025-12-19 23:21:20 +0300

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const start= async ()=>{
     try {
         const first = await readFilePromise('./content/frist.txt','utf8')
         const second = await readFilePromise('./content/second.txt','utf8')
-await writeFilePromise('./content/result=mind-grenade.txt',`THIS IS AWESOME:${first},${second}`,${flag:'a'})
+await writeFilePromise('./content/result=mind-grenade.txt',`THIS IS AWESOME:${first},${second}`,{ flag:'a'})
 
 
 console.log(first,second);
