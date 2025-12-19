8ccb488076a9c3ebc5836fe4a1e028ee97ffaa82
wakoya-tech <waaqoyyaadhaabaa@gmail.com>
2025-12-19 23:24:19 +0300

    add

--- 2-async-pattern/2-await-patter.js
+++ 2-async-pattern/2-await-patter.js
@@ -25,3 +25,27 @@ server.listen(5000, () => {
     console.log('Server listening on port 5000...')
 })
 console.log("welcome");
+/*const { readFile, writeFile } = require('fs').promises
+/*const util=require('util')
+const readFilePromise=util.promisify(readFile)
+const writeFilePromise=util.promisify(writeFile)
+*/
+/*
+const start= async ()=>{
+    try {
+        const first = await readFilePromise('./content/frist.txt','utf8')
+        const second = await readFilePromise('./content/second.txt','utf8')
+await writeFilePromise('./content/result=mind-grenade.txt',`THIS IS AWESOME:${first},${second}`,{ flag:'a'})
+
+
+console.log(first,second);
+    } 
+    catch (error) {
+        console.log(error)
+        
+    }
+}
+
+
+start()
+*/ 
